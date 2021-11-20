5b22f9c606e66baa505f04ced19d8af7758852ff
Mikkel Kaas <mkaas@openlab.dk>
2021-11-20 09:40:18 +0100

    LoginForm + CSS

--- includes/forms.class.php
+++ includes/forms.class.php
@@ -5,11 +5,11 @@
             print '
                 <div class="mb-3">
                     <label for="loginFormInput" class="form-label">Username</label>
-                    <input type="text" class="form-control" id="loginFormInput" placeholder="Username" />
+                    <input type="text" class="form-control" id="loginFormInput" placeholder="Username">
                 </div>
                 <div class="mb-3">
                     <label for="loginFormInput" class="form-label">Username</label>
-                    <input type="password" class="form-control" id="loginFormInput" placeholder="Password" />
+                    <input type="password" class="form-control" id="loginFormInput" placeholder="Password">
                 </div>
             ';
         }
